e9647282e4a065820dc2a3d5abe9e46ea229e6e3
Troy Lamerton <email": null>
2016-06-28 11:42:34 +1200

    Add iss tracking view

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var xhr = require('xhr')
 var greeting = require('./views/greeting.hbs')
+var satLocation = require('./views/satLocation.hbs')
 
 var endpoint = 'https://api.wheretheiss.at/v1/satellites'
 
@@ -7,11 +8,31 @@ xhr.get(endpoint, function (err, data) {
   if (err) {
     console.error(err)
   }
-
   // In case you're curious
   console.log(data.body) // FYI: data.body is a string
 
   // Replace 'Space' below with the response
   var target = document.getElementsByTagName('main')[0]
-  target.innerHTML = greeting({name: 'Space'})
+  target.innerHTML = greeting({name: JSON.parse(data.body)[0].name})
 })
+
+var button = document.getElementById('getButton')
+button.addEventListener('click', updateSatLocation)
+
+function updateSatLocation () {
+  var doc = document.getElementsByTagName('main')[0]
+
+  xhr.get(endpoint + '/25544', function (err, data) {
+    var altitude = data.body.altitude
+    // convert num to pixel string
+    altitude = altitude.toString() + 'px'
+    doc.innerHTML = satLocation({name: data.body.name, altitude: altitude, timestamp: 1436029902})
+
+    if (err) {
+      console.log('xhr error')
+      console.error(err)
+    }
+    console.log(data.body)
+  })
+
+}
